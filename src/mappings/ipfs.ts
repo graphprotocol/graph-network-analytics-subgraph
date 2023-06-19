e67c5cb6e4535c6f80f8db7b60b5c6a82390b061
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2023-06-19 13:44:06 -0300

    feat: FDS refactor

---
+++ src/mappings/ipfs.ts
@@ -0,0 +1,23 @@
+import { json, Bytes, dataSource, JSONValueKind, log } from '@graphprotocol/graph-ts'
+import {
+  GraphAccountMetadata
+} from '../types/schema'
+import { jsonToString } from './utils'
+
+export function handleGraphAccountMetadata(content: Bytes): void {
+  let graphAccountMetadata = new GraphAccountMetadata(dataSource.stringParam())
+  let tryData = json.try_fromBytes(content)
+  if (tryData.isOk) {
+    let data = tryData.value.toObject()
+    graphAccountMetadata.codeRepository = jsonToString(data.get('codeRepository'))
+    graphAccountMetadata.description = jsonToString(data.get('description'))
+    graphAccountMetadata.image = jsonToString(data.get('image'))
+    graphAccountMetadata.displayName = jsonToString(data.get('displayName'))
+    let isOrganization = data.get('isOrganization')
+    if (isOrganization != null && isOrganization.kind === JSONValueKind.BOOL) {
+      graphAccountMetadata.isOrganization = isOrganization.toBool()
+    }
+    graphAccountMetadata.website = jsonToString(data.get('website'))
+    graphAccountMetadata.save()
+  }
+}
