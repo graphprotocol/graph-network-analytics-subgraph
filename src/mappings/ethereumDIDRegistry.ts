29564e1baac3d9e9141daa46c83b4c447313a3d0
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2023-06-19 13:44:06 -0300

    feat: FDS refactor

--- src/mappings/ethereumDIDRegistry.ts
+++ src/mappings/ethereumDIDRegistry.ts
@@ -1,12 +1,12 @@
-import { json, ipfs, Bytes, JSONValueKind } from '@graphprotocol/graph-ts'
+import { Bytes } from '@graphprotocol/graph-ts'
 import { DIDAttributeChanged } from '../types/EthereumDIDRegistry/EthereumDIDRegistry'
+import { GraphAccountMetadata as GraphAccountMetadataTemplate } from '../types/templates'
+import { GraphAccount } from '../types/schema'
 
 import { addQm, createOrLoadGraphAccount } from './helpers'
-import { jsonToString } from './utils'
 
 export function handleDIDAttributeChanged(event: DIDAttributeChanged): void {
-  let id = event.params.identity.toHexString()
-  let graphAccount = createOrLoadGraphAccount(id, event.block.timestamp)
+  let graphAccount = createOrLoadGraphAccount(event.params.identity.toHexString(), event.block.timestamp)
   // OFFCHAIN_DATANAME = keccak256("GRAPH NAME SERVICE")
   // 0x72abcb436eed911d1b6046bbe645c235ec3767c842eb1005a6da9326c2347e4c
   if (
@@ -17,24 +17,9 @@ export function handleDIDAttributeChanged(event: DIDAttributeChanged): void {
     // called it directly, it could crash the subgraph
     let hexHash = changetype<Bytes>(addQm(event.params.value))
     let base58Hash = hexHash.toBase58()
-    graphAccount.metadataHash = event.params.value
+    graphAccount.metadata = base58Hash
+    graphAccount.save()
 
-    let ipfsData = ipfs.cat(base58Hash)
-    if (ipfsData !== null) {
-      let tryData = json.try_fromBytes(ipfsData as Bytes)
-      if (tryData.isOk) {
-        let data = tryData.value.toObject()
-        graphAccount.codeRepository = jsonToString(data.get('codeRepository'))
-        graphAccount.description = jsonToString(data.get('description'))
-        graphAccount.image = jsonToString(data.get('image'))
-        graphAccount.displayName = jsonToString(data.get('displayName'))
-        let isOrganization = data.get('isOrganization')
-        if (isOrganization != null && isOrganization.kind === JSONValueKind.BOOL) {
-          graphAccount.isOrganization = isOrganization.toBool()
-        }
-        graphAccount.website = jsonToString(data.get('website'))
-        graphAccount.save()
-      }
-    }
+    GraphAccountMetadataTemplate.create(base58Hash)
   }
 }
